cbdd0438b86a61dd616dda98ba388cad81d2109f
dmalkic <dmalkic@vvg.hr>
2022-05-14 16:35:00 +0200

    set production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
-
-config.action_mailer.default_url_options = {host: 'https://domagarticles.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'https://domagarticles.herokuapp.com/' }
 
 end
+
+
+
