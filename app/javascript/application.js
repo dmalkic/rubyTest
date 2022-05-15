c7f84259c57ac8056daa6f6a366697c2eec12689
dmalkic <dmalkic@vvg.hr>
2022-05-15 03:59:24 +0200

    mailer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 //= require bootstrap-sprockets
+//= require jquery3
+
+
 
 import "@hotwired/turbo-rails"
 import "controllers"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_150747) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_15_012105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_150747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
